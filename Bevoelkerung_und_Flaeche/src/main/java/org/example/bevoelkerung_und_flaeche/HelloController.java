644403dd782477d9ee11335a2df8250e20b8a292
akronste <akronste@htl-steyr.ac.at>
2026-01-21 12:23:09 +0100

    committ

--- Bevoelkerung_und_Flaeche/src/main/java/org/example/bevoelkerung_und_flaeche/HelloController.java
+++ Bevoelkerung_und_Flaeche/src/main/java/org/example/bevoelkerung_und_flaeche/HelloController.java
@@ -7,6 +7,12 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+    public HelloController(){
+        System.out.println("Hello");
+    }
+    public void initialize(){
+        welcomeText.setText("Welcome");
+    }
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
